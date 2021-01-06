f530dfc2c52774af80ce264c76c5d803c631c677
Siyeon Seo <siyeon.seo@lge.com>
2021-01-06 16:10:26 +0900

    Fix static analysis with MISRA 2008 Rule 5-0-11
    Change-Id: I45cfe375265a857d728918042f210c14f9b7ca0e

--- plugin/service.cpp
+++ plugin/service.cpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2012-2020 LG Electronics, Inc.
+// Copyright (c) 2012-2021 LG Electronics, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -506,8 +506,8 @@ void Service::setCallServiceName(QString& newServiceName) {
     if (!newServiceName.startsWith(strURISchemeDeprecated) && !newServiceName.startsWith(strURIScheme)) {
         newServiceName.prepend(strURIScheme);
     }
-    if (!newServiceName.endsWith('/')) {
-        newServiceName.append('/');
+    if (!newServiceName.endsWith(QChar('/'))) {
+        newServiceName.append(QChar('/'));
     }
     m_callServiceName = newServiceName;
 }
